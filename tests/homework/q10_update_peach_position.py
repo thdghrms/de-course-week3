fe6b2289e7254321ba705e141774eae223a932a0
moon <edu.moonduck@gmail.com>
2023-07-02 11:53:26 +0900

    Fix problem statement

--- tests/homework/q10_update_peach_position.py
+++ tests/homework/q10_update_peach_position.py
@@ -4,8 +4,8 @@
 # 위를 위반하면 채점이 제대로 되지 않아 0점 처리됩니다.
 
 # 아래에서 pass를 지우고 로직을 작성하세요
-Q10. Peach의 position을 senior engineer로 수정하는 로직을 작성하세요(10점)
-Query의 실행 결과 데이터베이스에서 Peach의 직위가 senior engineer로 수정되어 있어야 합니다.
+Q10. Peach의 position을 Senior engineer로 수정하는 로직을 작성하세요(10점)
+Query의 실행 결과 데이터베이스에서 Peach의 직위가 Senior engineer로 수정되어 있어야 합니다.
 
 # DB Connection 정보는 수강할 때 썼던 정보와 정확히 같습니다.
 # dbname: postgres
